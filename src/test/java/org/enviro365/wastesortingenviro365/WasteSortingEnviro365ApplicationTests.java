21232e07baaadc62c4be28be86f3eade637b1c63
Nonhle <nonhlee1@gmail.com>
2024-07-01 15:38:19 +0200

    packages created

---
+++ src/test/java/org/enviro365/wastesortingenviro365/WasteSortingEnviro365ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.enviro365.wastesortingenviro365;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteSortingEnviro365ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
